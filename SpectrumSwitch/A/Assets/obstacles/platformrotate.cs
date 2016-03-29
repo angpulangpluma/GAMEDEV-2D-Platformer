24d9ffb7fc5ebb66e24b1b19d7236b386c2c5ad3
iamame10 <kenneth.devera10@gmail.com>
2016-03-29 16:09:32 +0800

    hi

---
+++ SpectrumSwitch/A/Assets/obstacles/platformrotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class platformrotate : MonoBehaviour {
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * -2);
+
+    }
+}
