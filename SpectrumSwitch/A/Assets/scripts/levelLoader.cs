bea678434370604d5f9f6a291d6de50625878261
iamame10 <kenneth.devera10@gmail.com>
2016-04-01 10:50:06 +0800

    hi

---
+++ SpectrumSwitch/A/Assets/scripts/levelLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour {
+
+    public string loadit;
+    public bool what;
+
+	void Start()
+    {
+
+        what = false;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col4)
+    {
+        if(col4.gameObject.name == "all color ball")
+        {
+            what = true;
+
+        }
+
+    }
+    
+	void Update () {
+
+        if(what == true)
+        SceneManager.LoadScene(loadit);
+	}
+}
---
+++ SpectrumSwitch/A/Assets/scripts/mainMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public string startlevel;
+    public string restartgame;
+    public string home;
+    public string setting;
+    public string inst;
+
+    public void Play()
+    {
+
+        SceneManager.LoadScene(startlevel);
+
+    } 
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(restartgame);
+
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(home);
+
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(setting);
+
+    }
+
+    public void Inst()
+    {
+        SceneManager.LoadScene(inst);
+
+    }
+
+    public void exitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+
+}
