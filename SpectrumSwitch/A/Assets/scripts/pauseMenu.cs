7dc9322eacb2fd26e3da227f799fca944a259f05
viciouzkitten <aileen.sabellon@gmail.com>
2016-04-01 14:07:42 +0800

    pause menu

--- SpectrumSwitch/A/Assets/scripts/pauseMenu.cs
+++ SpectrumSwitch/A/Assets/scripts/pauseMenu.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class pauseMenu : MonoBehaviour {
+    public string restartgame;
+    public string home;
+    public bool isPaused;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    public GameObject pausedMenuCanvas;
+
+   	// Update is called once per frame
 	void Update () {
+        if(isPaused)
+        {
+            pausedMenuCanvas.SetActive(true);
+        }else{
+            pausedMenuCanvas.SetActive(false);
+        }
 	
 	}
+    public void Pause()
+    {
+        isPaused = true;
+    }
+    public void Resume()
+    {
+        isPaused = false;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(restartgame);
+
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(home);
+
+    }
 }
