ee9b2888e8eae7d5a11d0843c12027e79be2e069
viciouzkitten <aileen.sabellon@gmail.com>
2016-04-01 18:09:53 +0800

    cleaned interface

--- SpectrumSwitch/A/Assets/scripts/pauseMenu.cs
+++ SpectrumSwitch/A/Assets/scripts/pauseMenu.cs
@@ -7,61 +7,18 @@ public class pauseMenu : MonoBehaviour {
     public string home;
     public bool isPaused;
 
-<<<<<<< HEAD
-    public string restra;
-    public string home;
-    public bool paused;
-    public GameObject pausing;
-
-    public void Update()
-    {
-        if(paused == true)
-        {
-            pausing.SetActive(true);
-            Time.timeScale = 0f;
-
-        }
-
-        else if (paused == false)
-        {
-            pausing.SetActive(false);
-            Time.timeScale = 1f;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-            paused = !paused;
-
-
-    }
-
-    public void pass()
-    {
-
-        paused = true;
-
-    }
-
-    void Start()
-    {
-
-        paused = false;
-
-    }
-
-    public void Game()
-    {
-
-         paused = false;
-=======
     public GameObject pausedMenuCanvas;
 
-   	// Update is called once per frame
+   
 	void Update () {
         if(isPaused)
         {
             pausedMenuCanvas.SetActive(true);
-        }else{
+            Time.timeScale = 0f;
+        }
+        else{
             pausedMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
         }
 	
 	}
@@ -69,34 +26,25 @@ public class pauseMenu : MonoBehaviour {
     {
         isPaused = true;
     }
+
     public void Resume()
     {
         isPaused = false;
     }
-    public void Restart()
-    {
-        SceneManager.LoadScene(restartgame);
->>>>>>> origin/master
-
-    }
+  
 
     public void Home()
     {
-<<<<<<< HEAD
 
         SceneManager.LoadScene(home);
 
     }
 
-    public void restart()
+    public void Restart()
     {
 
-        SceneManager.LoadScene(restra);
+        SceneManager.LoadScene(restartgame);
     }
 
-=======
-        SceneManager.LoadScene(home);
 
-    }
->>>>>>> origin/master
 }
