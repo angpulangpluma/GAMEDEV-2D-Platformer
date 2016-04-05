27a401a08e622d200cc9cf72b7eb35021283d773
iamame10 <kenneth.devera10@gmail.com>
2016-04-06 00:56:42 +0800

    hi

---
+++ SpectrumSwitch/A/Assets/scripts/buttonMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class buttonMovement : MonoBehaviour, IEventSystemHandler
+{
+    public GameObject character;
+    public float maxSpeed = 20f;
+    bool buttonHeld = false;
+    public Button rytButton;
+    public Button lepButton;
+    public Button upButton;
+    public GameObject buts;
+
+    public void pressed(BaseEventData eventData)
+    {
+        buttonHeld = true;
+
+    }
+    public void notpressed(BaseEventData eventData)
+    {
+        buttonHeld = false;
+    }
+    void Start()
+    {
+        character = GameObject.FindWithTag("Player");
+    }
+    public void Update()
+    {
+        buts = EventSystem.current.currentSelectedGameObject;
+        if (buttonHeld == true)
+        {
+            if(buts.name == "Right Button")
+                character.transform.position += Vector3.right * maxSpeed * Time.deltaTime;
+
+            else if(buts.name == "Left Button")
+               character.transform.position += Vector3.left * maxSpeed * Time.deltaTime;
+
+            if(buts.name == "Jump Button")
+               character.transform.position += Vector3.up * maxSpeed * Time.deltaTime;
+        }
+
+    }
+}
